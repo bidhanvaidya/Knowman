a4cbd96dcca95834b2e2abdf48257153cde137ce
rails <rails@ubuntu.(none)>
2011-09-10 16:21:06 +1000

    Kaushal Devise install

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910050450) do
+ActiveRecord::Schema.define(:version => 20110910060142) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "project_id"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(:version => 20110910050450) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -53,6 +52,25 @@ ActiveRecord::Schema.define(:version => 20110910050450) do
     t.string   "name"
     t.integer  "staff_numbers"
     t.integer  "capital"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
