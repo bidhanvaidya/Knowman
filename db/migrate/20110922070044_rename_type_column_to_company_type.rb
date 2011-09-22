4cadb0477acc09a4eb93567fc3e7cbd9a01f8634
Lu Chen <luchen@Lu-Chens-MacBook-Pro.local>
2011-09-22 22:01:40 +1000

    fix rename

--- db/migrate/20110922070044_rename_type_column_to_company_type.rb
+++
@@ -1,9 +0,0 @@
-class RenameTypeColumnToCompanyType < ActiveRecord::Migration
-  def up
-    rename_column :company, :type, :company_type
-  end
-
-  def down
-    rename_column :company, :company_type, :type
-  end
-end
