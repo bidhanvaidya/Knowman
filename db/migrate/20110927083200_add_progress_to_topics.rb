0e2ed051412ab24a6b0d5210aa80371f79981c82
rails <rails@ubuntu.(none)>
2011-10-01 12:32:59 +1000

    --Bidhan ----clearing errors--Bidhan

--- db/migrate/20110927083200_add_progress_to_topics.rb
+++
@@ -1,8 +0,0 @@
-class AddProgressToTopics < ActiveRecord::Migration
-  def up
-		add_column :topics, :progress, :integer, default => 0
-  end
-	def down
-		remove_column :topics, :progress	
-	end
-end
--- db/migrate/20110927104036_add_progress_to_topics_with_default.rb
+++
@@ -1,5 +0,0 @@
-class AddProgressToTopicsWithDefault < ActiveRecord::Migration
-  def change
-		add_column :topics, :progress, :integer, :default=>'0'
-  end
-end
