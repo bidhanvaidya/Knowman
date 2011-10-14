874bc9da784752a6b1bf0979a50191832f08cc00
rmse <rmse@Richard-Masseys-MacBook.local>
2011-10-15 00:42:59 +1100

    Richard: Cleared 'invalid association' errors from rake erd.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
   has_many :employment_statuses
   has_many :companies, :through => :employment_statuses
+  has_many :assignments
   has_many :products, :through => :assignments
 end
