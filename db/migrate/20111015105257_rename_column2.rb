6a6392b2c6f8eaffab44e2e222a175d262640290
rails <rails@ubuntu.(none)>
2011-10-16 16:03:30 +1100

    bidhan Vaidya==> plz pull this

--- db/migrate/20111015105257_rename_column2.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumn2 < ActiveRecord::Migration
-  def up
-    rename_column :employment_statuses, :person_id, :person_permalink
-  end
-
-  def down
-    rename_column :employment_statuses, :person_permalink, :person_id
-  end
-end
